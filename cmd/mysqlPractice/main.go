86821c512ffa58ed4acfe60c984e5114410f8dc0
Ronnie Lin <ronnie_lin@symphox.net>
2021-06-17 22:56:48 +0800

    add mysqlPractice

--- cmd/mysqlPractice/main.go
+++ cmd/mysqlPractice/main.go
@@ -11,14 +11,27 @@ import (
 
 func main() {
 
-	db, err := sql.Open("mysql", "root:my-password@/test?charset=utf8")
+	EMPNO, ENAME, JOB, DEPTNO := 555, "第5個", "NO5", 5
+	// var EMPNO int16 = 333
+	// var ENAME string = "第3個"
+	// var JOB string = "NO3"
+	// var DEPTNO int16 = 3
+
+	InsertEMP(&EMPNO, ENAME, JOB, DEPTNO)
+
+}
+
+func InsertEMP(EMPNO *int, ENAME string, JOB string, DEPTNO int) {
+	db, err := sql.Open("mysql", "root:my-password@/test")
 	checkErr(err, db)
 
 	//插入資料
-	stmt, err := db.Prepare("INSERT INTO EMP (EMPNO , ENAME, JOB ,deptno ) VALUE (?,?,?,?)")
-	checkErr(err, db)
+	stmt, err := db.Prepare("INSERT INTO EMP (EMPNO , ENAME, JOB ,DEPTNO ) VALUE (?,?,?,?)")
+	if err != nil {
+		checkErr(err, db)
+	}
 
-	res, err := stmt.Exec("1111", "測試人員1", "Engineer", 20)
+	res, err := stmt.Exec(*EMPNO, ENAME, JOB, DEPTNO)
 	checkErr(err, db)
 
 	id, err := res.LastInsertId()
@@ -31,19 +44,20 @@ func main() {
 
 	fmt.Println(affect)
 
-	defer func() {
-		db.Close()
-		fmt.Print("關閉36")
-	}()
+	db.Close()
+	fmt.Print("關閉36")
 }
 
 func checkErr(err error, db *sql.DB) {
 	if err != nil {
+
+		defer func() {
+			db.Close()
+			fmt.Print("關閉47")
+		}()
+
+		fmt.Print(err.Error())
 		panic(err)
 	}
 
-	defer func() {
-		db.Close()
-		fmt.Print("關閉47")
-	}()
 }
