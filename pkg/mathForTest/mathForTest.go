1afbd72128878bcc2df4f37eae3383b09f198e88
Ronnie Lin <ronnie_lin@symphox.net>
2021-06-23 00:25:52 +0800

    unit test adjust

--- pkg/mathForTest/mathForTest.go
+++ pkg/mathForTest/mathForTest.go
@@ -4,6 +4,6 @@ func Plus(a, b int) int {
 	return a + b
 }
 
-func minus(a, b int) int {
+func Minus(a, b int) int {
 	return a - b
 }
--- test/mathForTest/mathForTest_test.go
+++ test/mathForTest/mathForTest_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestMathPlus(t *testing.T) {
-	fmt.Println("開始測試加法")
+	fmt.Println("開始測試 加法")
 
 	var a, b = 2, 3
 
@@ -17,4 +17,20 @@ func TestMathPlus(t *testing.T) {
 	if result != a+b {
 		t.Error("加法錯誤")
 	}
+
+	t.Logf("又錯了，還不好好寫程式")
+}
+
+func TestMathMinus(t *testing.T) {
+	fmt.Println("開始測試 減法")
+
+	var a, b = 5, 1
+
+	result := mathForTest.Minus(a, b)
+
+	if result != a-b {
+		t.Error("減法錯誤")
+	}
+
+	t.Logf("又錯了，還不好好寫程式")
 }
