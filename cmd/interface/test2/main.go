a39cd7b62759eda1e90a3a12a7ccbe12c06317f2
Ronnie <ronnielin8862@gmail.com>
2021-08-15 18:42:51 +0700

    interface

---
+++ cmd/interface/test2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Database interface {
+	Read(string) string
+	Write(string)
+}
+
+// MySQL 會實作 Database 介面。
+type MySQL struct{}
+
+func (m MySQL) Read(s string) string {
+	fmt.Println("Mysql的 ", s)
+	return s
+}
+func (m MySQL) Write(string) {}
+
+// MongoDB 會實作 Database 介面。
+type MongoDB struct{}
+
+func (m MongoDB) Read(s string) string {
+	fmt.Println("Mongo的 ", s)
+	return s
+}
+func (m MongoDB) Write(string) {}
+
+// New 會將接收到的物件以 Database 實作，並且呼叫相關函式對資料庫進行操作。
+func New(db Database, s string) {
+	// 讀取資料庫。
+	db.Read(s)
+	// 寫入資料庫。
+	db.Write(s)
+}
+
+func main() {
+	// var mm MySQL
+
+	s := "我叫你吹"
+	// 將建構體傳入 New 就會被實作成 Database。
+	New(MySQL{}, s)
+	New(MongoDB{}, s)
+
+}
