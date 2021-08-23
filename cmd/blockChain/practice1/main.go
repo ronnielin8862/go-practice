bf42a276ff75695c8db8524b573ba214c4a6b6ca
Ronnie <ronnie@EricdeMac-mini.local>
2021-08-23 11:10:08 +0700

    block chain - practice1

---
+++ cmd/blockChain/practice1/main.go
@@ -0,0 +1,11 @@
+package main
+
+// import
+
+func main() {
+	// 	client, err := Connect("http://localhost:8545")
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 	}
+	// 	blockNumber, err := client.GetBlockNumber(context.TODO())
+}
---
+++ pkg/blockChain/practice1/createConn.go
@@ -0,0 +1,33 @@
+package practice1
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+// Client defines typed wrappers for the Ethereum RPC API.
+type Client struct {
+	rpcClient *rpc.Client
+	EthClient *ethclient.Client
+}
+
+// Connect creates a client that uses the given host.
+func Connect(host string) (*Client, error) {
+	rpcClient, err := rpc.Dial(host)
+	if err != nil {
+		return nil, err
+	}
+	ethClient := ethclient.NewClient(rpcClient)
+	return &Client{rpcClient, ethClient}, nil
+}
+
+// GetBlockNumber returns the block number.
+func (ec *Client) GetBlockNumber(ctx context.Context) (*big.Int, error) {
+	var result hexutil.Big
+	err := ec.rpcClient.CallContext(ctx, &result, "eth_blockNumber")
+	return (*big.Int)(&result), err
+}
