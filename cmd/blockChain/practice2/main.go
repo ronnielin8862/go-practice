c6562ec6f0ede07a02b71ef63a05a59a16298da2
Ronnie <ronnie@Mac-mini.local>
2021-08-25 15:04:20 +0700

    nothing to changes?

--- cmd/blockChain/practice2/main.go
+++ cmd/blockChain/practice2/main.go
@@ -19,29 +19,29 @@ func main() {
 	fmt.Println("Welcome to our Blockchain project.")
 	fmt.Println("Enter h for help")
 
-	var (
-		op string
-	)
+	var operations string
 
-	NewBlockchain := bc.CreateBlockchain() // 新增一個區塊鏈
+	NewBlockchain := bc.CreateBlockchain() // 新增 第一個 初始區塊鏈
+
 	for {
-		fmt.Scanln(&op)
-		if op == "h" {
+		fmt.Scanln(&operations)
+
+		if operations == "h" { // 顯示使用方法
 			fmt.Println("Printing the help")
-			Help() // 顯示使用方法
-		} else if op == "1" {
+			Help()
+		} else if operations == "1" { //開始創建 後續區塊鏈
 			fmt.Println("Entering your data:")
 			reader := bufio.NewReader(os.Stdin)
 			data, _, _ := reader.ReadLine() // 讀一整行 input
 			NewBlockchain.AddBlock(data)    // 利用 input 作為 data 來創建區塊鏈
-		} else if op == "2" {
+		} else if operations == "2" {
 			for _, block := range NewBlockchain.Blocks {
 				fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 				fmt.Printf("Data: %s\n", block.Data)
 				fmt.Printf("Hash: %x\n", block.Hash)
 				fmt.Println()
 			} // 查詢資料
-		} else if op == "3" {
+		} else if operations == "3" {
 			break
 		} else {
 			fmt.Println("Please Enter h, 1, 2, 3")
