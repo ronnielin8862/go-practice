497e62ce713e1470f7634ec606b814b7893240a9
ronnie <ronnie.h@iscmango.com>
2022-06-17 09:29:08 +0800

    nats - 透過JetStream 達到mq持久化

--- cmd/mq/nat/publisher/main.go
+++ cmd/mq/nat/publisher/main.go
@@ -1,18 +1,67 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/nats-io/nats.go"
+	"log"
+	"strconv"
 	"time"
 )
 
+type ChatHistory struct {
+	Id         int64  `json:"id" gorm:"primaryKey;autoIncrement"` // bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	Uid        int    `json:"uid"`                                //int(11) DEFAULT NULL COMMENT '用户id',
+	RoomId     int    `json:"room_id"`                            //varchar(100) DEFAULT NULL COMMENT '房间id',
+	CreateTime int64  `json:"create_time"`                        //datetime DEFAULT NULL,
+	Content    string `json:"content"`
+}
+
+type SendGiftReq struct {
+	Anchorid int   `json:"anchorid" validate:"required"`
+	Giftid   int   `json:"giftid" validate:"required"`
+	Liveid   int64 `json:"liveid"`
+	Count    int   `json:"count"`
+	Uid      int
+}
+
 func main() {
 	nc, _ := nats.Connect("nats://127.0.0.1:4222")
+	jetStreamPubTestForDDU(nc)
+}
+
+func generalPublish(nc *nats.Conn) {
 
 	// 发布-订阅 模式，向 test1 发布一个 `Hello World` 数据
-	_ = nc.Publish("test1", []byte("Hello World"))
+	chat := ChatHistory{
+		Id:         666,
+		Uid:        555,
+		RoomId:     444,
+		CreateTime: 1655273431,
+		Content:    "鬼塚英吉",
+	}
+
+	chatMarshal, err := json.Marshal(chat)
+	if err != nil {
+		return
+	}
+	_ = nc.Publish("chatRecordChannel", []byte(chatMarshal))
+
+	gift := SendGiftReq{
+		Anchorid: 666,
+		Giftid:   555,
+		Liveid:   444,
+		Count:    666,
+		Uid:      777,
+	}
+	giftMarshal, err := json.Marshal(gift)
+	if err != nil {
+		return
+	}
+	_ = nc.Publish("sendGiftChannel", []byte(giftMarshal))
 
 	// 队列 模式，发布是一样的，只是订阅不同，向 test2 发布一个 `Hello zngw` 数据
-	_ = nc.Publish("test2", []byte("Hello zngw"))
+	//_ = nc.Publish("test.2", []byte("鬼塚英吉"))
 
 	// 请求-响应， 向 test3 发布一个 `help me` 请求数据，设置超时间3秒，如果有多个响应，只接收第一个收到的消息
 	//msg, err := nc.Request("test3", []byte("help me"), 3*time.Second)
@@ -21,5 +70,144 @@ func main() {
 	//} else {
 	//	fmt.Printf("help answer : %s\n", string(msg.Data))
 	//}
+	// 清空緩衝
+	err = nc.Flush()
+	if err != nil {
+		log.Fatal("清空失敗")
+	}
+
 	time.Sleep(3 * time.Second)
+}
+
+func replyPublish(nc *nats.Conn) {
+
+	reply := nats.NewInbox()
+	i := 0
+
+	for {
+		// 自動建立一個唯一 subject
+		// 發送訊息
+		//nc.Publish("subject", []byte("Hello World!"))
+
+		ms := "Hello world  " + strconv.Itoa(i)
+		err := nc.PublishRequest("subject", reply, []byte(ms))
+		if err != nil {
+			log.Fatal("送不出去")
+		}
+		fmt.Println("send ", ms)
+
+		i++
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func jetStreamPublish(nc *nats.Conn) {
+	js, _ := nc.JetStream()
+	err := js.DeleteStream("Stream名稱_pub3")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 建立 Stream
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name: "Stream名稱_pub3",
+		Subjects: []string{
+			"testTopic8.*", // 支援 wildcard
+		},
+		Storage:   nats.FileStorage,     // 儲存的方式 (預設 FileStorage)
+		Retention: nats.WorkQueuePolicy, // 保留的策略
+		Discard:   nats.DiscardOld,      // 丟棄的策略
+		// ...
+	})
+	if err != nil {
+		log.Fatalf("建立 Stream 失敗: %v", err)
+	}
+
+	i := 0
+
+	for {
+
+		ms := "Hello world  " + strconv.Itoa(i)
+		_, err = js.Publish("testTopic8.12345", []byte(ms))
+		if err != nil {
+			log.Fatal("送不出去, err = ", err)
+		}
+		fmt.Println("send ", ms)
+
+		i++
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func jetStreamPubTestForDDU(nc *nats.Conn) {
+	js, _ := nc.JetStream()
+
+	_, err := js.AddStream(&nats.StreamConfig{
+		Name: "webapiStreamChannel",
+		Subjects: []string{
+			"testTopic8.*", "chatRecordChannel", "sendGiftChannel", // 支援 wildcard
+		},
+		Storage:   nats.FileStorage,     // 儲存的方式 (預設 FileStorage)
+		Retention: nats.WorkQueuePolicy, // 保留的策略
+		Discard:   nats.DiscardOld,      // 丟棄的策略
+		// ...
+	})
+	if err != nil {
+		log.Fatalf("建立 Stream 失敗: %v", err)
+	}
+
+	i := 0
+	for {
+		// 发布-订阅 模式，向 test1 发布一个 `Hello World` 数据
+		chat := ChatHistory{
+			Id:         int64(i),
+			Uid:        555,
+			RoomId:     444,
+			CreateTime: 1655273431,
+			Content:    "鬼塚英吉",
+		}
+
+		chatMarshal, err := json.Marshal(chat)
+		if err != nil {
+			return
+		}
+		_, err = js.Publish("chatRecordChannel", []byte(chatMarshal))
+		if err != nil {
+			log.Fatal("送不出去, err = ", err)
+		}
+		fmt.Println("send chat ", i)
+
+		gift := SendGiftReq{
+			Anchorid: i,
+			Giftid:   555,
+			Liveid:   444,
+			Count:    666,
+			Uid:      777,
+		}
+		giftMarshal, err := json.Marshal(gift)
+		if err != nil {
+			return
+		}
+		_, err = js.Publish("sendGiftChannel", []byte(giftMarshal))
+		if err != nil {
+			log.Fatal("送不出去, err = ", err)
+		}
+		fmt.Println("send gift ", i)
+
+		// 队列 模式，发布是一样的，只是订阅不同，向 test2 发布一个 `Hello zngw` 数据
+		//_ = nc.Publish("test.2", []byte("鬼塚英吉"))
+
+		// 请求-响应， 向 test3 发布一个 `help me` 请求数据，设置超时间3秒，如果有多个响应，只接收第一个收到的消息
+		//msg, err := nc.Request("test3", []byte("help me"), 3*time.Second)
+		//if err != nil {
+		//	fmt.Println(err)
+		//} else {
+		//	fmt.Printf("help answer : %s\n", string(msg.Data))
+		//}
+
+		time.Sleep(2 * time.Second)
+		i++
+	}
 }
--- cmd/mq/nat/subscribe/main.go
+++ cmd/mq/nat/subscribe/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/nats-io/nats.go"
 	"log"
 	"os"
@@ -10,6 +12,13 @@ import (
 func main() {
 	// Create server connection
 	nc, _ := nats.Connect("nats://127.0.0.1:4222")
+	defer nc.Close()
+	jetStream(nc)
+}
+
+func generalSubscript(nc *nats.Conn) {
+	// Create server connection
+	nc, _ = nats.Connect("nats://127.0.0.1:4222")
 	defer nc.Close()
 
 	mcbAny := func(msg *nats.Msg) {
@@ -38,10 +47,128 @@ func main() {
 		}
 	}()
 
+	// only for here, it is no need in general project
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-
 	<-c
 	Sub1Cb.Unsubscribe()
 	Sub2Cb.Unsubscribe()
+}
+
+func replySubscript(nc *nats.Conn) {
+
+	var Sub1Cb *nats.Subscription
+
+	// 接收訊息
+	Sub1Cb, err := nc.Subscribe("subject", func(msg *nats.Msg) {
+		fmt.Println("收到了", string(msg.Data))
+
+		msg.Respond([]byte("i got it")) // 生產者會監聽 reply，來確認消費者有沒有收到
+	})
+	if err != nil {
+		log.Fatal("訂閱失敗")
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	Sub1Cb.Unsubscribe()
+}
+
+func jetStream(nc *nats.Conn) {
+
+	// Create server connection
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatalf("取得 JetStream 的 Context 失敗: %v", err)
+	}
+
+	// 建立 Stream
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name: "Stream名稱_pub1",
+		Subjects: []string{
+			"testTopic8.*", // 支援 wildcard
+		},
+		Storage:   nats.FileStorage,     // 儲存的方式 (預設 FileStorage)
+		Retention: nats.WorkQueuePolicy, // 保留的策略
+		Discard:   nats.DiscardOld,      // 丟棄的策略
+		// ...
+	})
+	if err != nil {
+		log.Fatalf("建立 Stream 失敗: %v", err)
+	}
+
+	mcbAny := func(msg *nats.Msg) {
+		log.Println("receive:", string(msg.Data))
+
+	}
+
+	_, err = js.Subscribe("testTopic8.*", mcbAny)
+	if err != nil {
+		log.Println("queue subscribe testTopic.*:", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+}
+
+func jetStreaForDDU(nc *nats.Conn) {
+	js, err := nc.JetStream()
+
+	// build stream
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name: "webapiStreamChannel",
+		Subjects: []string{
+			"chatRecordChannel", "sendGiftChannel", // 支援 wildcard
+		},
+		Storage:   nats.FileStorage,     // 儲存的方式 (預設 FileStorage)
+		Retention: nats.WorkQueuePolicy, // 保留的策略
+		Discard:   nats.DiscardOld,      // 丟棄的策略
+	})
+	chatHistoryProc := func(msg *nats.Msg) {
+		var chatHistory ChatHistory
+		err = json.Unmarshal(msg.Data, &chatHistory)
+		if err != nil {
+			fmt.Println("SubMessage chatRecord InsertChatHistory err [%v]", err)
+		}
+		fmt.Println("chatHistory: ", chatHistory)
+		//repository.InsertChatHistory(&chatHistory)
+	}
+
+	sendGiftProc := func(msg *nats.Msg) {
+		var sendGiftReq SendGiftReq
+		json.Unmarshal(msg.Data, &sendGiftReq)
+		fmt.Println("sendGiftReq: ", sendGiftReq)
+		//_, err = services.SendGift(&sendGiftReq, sendGiftReq.Uid)
+		if err != nil {
+			fmt.Println("发送礼物失败%v,礼物数据:%+v", err, msg.Data)
+		}
+	}
+
+	_, err = js.Subscribe("chatRecordChannel", chatHistoryProc)
+	if err != nil {
+		fmt.Println("SubMessage ChatRecord err [%v]", err)
+	}
+	_, err = js.Subscribe("sendGiftChannel", sendGiftProc)
+	if err != nil {
+		fmt.Println("SubMessage SendGift err [%v]", err)
+	}
+}
+
+type SendGiftReq struct {
+	Anchorid int   `json:"anchorid" validate:"required"`
+	Giftid   int   `json:"giftid" validate:"required"`
+	Liveid   int64 `json:"liveid"`
+	Count    int   `json:"count"`
+	Uid      int
+}
+
+type ChatHistory struct {
+	Id         int64  `json:"id" gorm:"primaryKey;autoIncrement"` // bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	Uid        int    `json:"uid"`                                //int(11) DEFAULT NULL COMMENT '用户id',
+	RoomId     int    `json:"room_id"`                            //varchar(100) DEFAULT NULL COMMENT '房间id',
+	CreateTime int64  `json:"create_time"`                        //datetime DEFAULT NULL,
+	Content    string `json:"content"`
 }
