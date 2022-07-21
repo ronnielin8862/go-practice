11adf5fc7b06cba8827f4f1ada8e27b230526eed
ronnie <ronnie.h@iscmango.com>
2022-07-21 14:01:47 +0800

    新增 Cobra範例

--- cmd/cobraTest.go
+++ cmd/cobraTest.go
@@ -13,9 +13,22 @@ func init() {
 			Short: "Short",
 			Long:  "Long",
 			Run: func(cmd *cobra.Command, args []string) {
-				fmt.Println("RUN func")
+				fmt.Println("RUN fuck")
 			},
 		}
 	)
 	rootCmd.AddCommand(serveCmd)
+
+	// 實測 無法一次執行兩個subcommand
+	var serveCmd2 = &cobra.Command{
+		Use:   "serve2",
+		Short: "Short2",
+		Long:  "Long2",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("RUN fuck 2")
+		},
+	}
+	rootCmd.AddCommand(serveCmd2)
+
+	// 實作可參考我寫的 chat_room包 的match-live
 }
