882c63f11f4f9cf24c64bd6e1b9171d0d748775c
Ronnie <ronnielin8862@gmail.com>
2022-08-13 22:34:05 +0800

    新增 context 操作

--- cmd/basic/contex/main.go
+++ cmd/basic/contex/main.go
@@ -8,28 +8,35 @@ import (
 
 func main() {
 	ctx := context.WithValue(context.Background(), "keyA", "valueA")
-	//ctx2, _ := context.WithDeadline(ctx, time.Now().Add(time.Second*2)) // 设置超时时间點
-	ctx2, _ := context.WithTimeout(ctx, time.Second*5) // 设置超時倒數時間
-	go layer1(ctx2)
-	select {
-	case <-ctx2.Done():
-		fmt.Println("main done")
-	}
-	time.Sleep(time.Second * 3)
+	//ctx, cancel = context.WithCancel(ctx)
+	ctx, _ = context.WithDeadline(ctx, time.Now().Add(time.Second*5)) // 设置超时时间點
+	//ctx, _ = context.WithTimeout(ctx, time.Second*5) // 设置超時倒數時間
+	go layer1(ctx)
+	//select {
+	//case <-ctx.Done():
+	//	fmt.Println("main done")
+	//}
+
+	time.Sleep(time.Second * 10)
 }
 
 func layer1(ctx context.Context) {
 	fmt.Println("layer1")
-	layer2(ctx)
-	select {
-	case <-ctx.Done():
-		fmt.Println("layer1 done")
-	}
+	go layer2(ctx)
+	go func() {
+		select {
+		case <-ctx.Done():
+			fmt.Println("layer1 done")
+		}
+	}()
 }
 
 func layer2(ctx context.Context) {
 	fmt.Println("layer2")
-	layer3(ctx)
+	ctx2, cancel := context.WithCancel(ctx)
+	go layer3(ctx2) // 取消context
+	time.Sleep(time.Second * 4)
+	cancel() // 取消自身協程以及子協程
 	select {
 	case <-ctx.Done():
 		fmt.Println("layer2 done")
@@ -42,7 +49,7 @@ func layer3(ctx context.Context) {
 	if deadline, ok := ctx.Deadline(); ok {
 		fmt.Println("deadline = ", deadline)
 	}
-	layer4(ctx)
+	go layer4(ctx)
 	select {
 	case <-ctx.Done():
 		fmt.Println("layer3 done")
@@ -57,4 +64,5 @@ func layer4(ctx context.Context) {
 	} else {
 		fmt.Println("layer4 err = nil")
 	}
+
 }
