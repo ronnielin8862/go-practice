50dc5847ff616a4e6e9c12aba90b70eefdd11449
Ronnie <ronnielin8862@gmail.com>
2022-08-28 15:58:27 +0800

    函式計算時間

---
+++ cmd/basic/time/executeTime/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var t int = 50000
+	fmt.Println("n:", sum(t))
+
+	fmt.Println("n2: ", sum2(t))
+}
+
+func sum(n int) int {
+	// defer 會先進入執行，待離開function時返回
+	defer fmt.Println("defer return: ", timeCost())
+	a := time.Now()
+
+	var s int = 0
+	for i := 0; i < n; i++ {
+		s += i
+	}
+	sc := time.Since(a)
+	println(sc.Nanoseconds())
+	return s
+}
+
+func timeCost() int {
+	fmt.Println("start")
+	return 999
+}
+
+func sum2(n int) int {
+	defer timeCost2()() // 不加上第二個"()", 只回返回方法卻不執行。 如果print timeCost2()，會得到function address。
+	var s int = 0
+	for i := 0; i < n; i++ {
+		s += i
+	}
+	return s
+}
+
+func timeCost2() func() {
+	startTime := time.Now()
+	return func() {
+		t := time.Since(startTime)
+		fmt.Println("time cost2: ", t.Nanoseconds())
+	}
+}
