253a501f9f214ee339f5729b1a4d65d3096ebad9
ronnie <ronnie.h@iscmango.com>
2022-10-14 18:53:25 +0800

    rabbit mq

--- cmd/temp/main.go
+++ cmd/temp/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type lineupResp struct {
@@ -11,19 +10,7 @@ type lineupResp struct {
 }
 
 func main() {
-	// git 1
-	// git 2
-	t := time.Now()
-	today := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
-
-	fmt.Println(today)
-
-	y := time.Now().AddDate(0, 0, -1)
-	yesterday := time.Date(y.Year(), y.Month(), y.Day(), 0, 0, 0, 0, t.Location()).Unix()
-	fmt.Println(yesterday)
-
-	aa := fmt.Sprintf("select match_id , user_id from db_match_favorite where match_id in ( select match_id from db_matches_schedule where match_time >= %v and match_time < %v )", yesterday, today)
-	fmt.Println(aa)
+	fmt.Println("2222 = ", 50/100)
 }
 
 func testReturn(s string) string {
