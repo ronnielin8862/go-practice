828346c1a36c52b6788b919e1e2e2bd791747f53
ronnie <ronnielin8862@gmail.com>
2023-01-02 21:25:20 +0800

    a

--- cmd/temp/main.go
+++ cmd/temp/main.go
@@ -21,9 +21,9 @@ var ch chan struct{}
 
 func main() {
 	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
+	//fmt.Println(b)
+	//fmt.Println(c)
+	//fmt.Println(d)
 	//fmt.Println(e)
 }
 
